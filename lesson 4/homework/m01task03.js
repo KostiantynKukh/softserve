a85485c5aefeffd21380697edda712e0eb52c165
KostiantynKukh <kkuh1993@gmail.com>
2020-03-19 14:54:14 +0200

    added folder lesson 4

---
+++ lesson 4/homework/m01task03.js
@@ -0,0 +1,12 @@
+﻿// Task03 - Write a code that returns concatenation of 2 inputted strings
+// Use space (" ") as a string separator
+// For example: 
+// first is a string "Hello", second is a string "world!"
+// You should return a string "Hello world!"
+
+function task03(first, second) {
+    let concat = first + " " + second;
+    return concat;
+    // TODO: Write your code here
+}
+console.log(task03("hello","world"));
---
+++ lesson 4/homework/m01task04.js
@@ -0,0 +1,16 @@
+﻿// Task04 - Write a code that returns string converted from 3 inputted digits
+// Don't use a separators in string
+// first, second and third are inputed values 
+// For example: 
+// Inputed value are 2, 5 and 7
+// You should return a next string - "257"
+
+function task04(first, second, third) {
+    let f = first.toString(),
+        s = second.toString(),
+        t = third.toString();
+    let sum = f + s + t;
+    return sum;
+    // TODO: Write your code here
+}
+console.log(task04(3,5,8));
